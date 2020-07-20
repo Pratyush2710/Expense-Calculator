c2372fb7b20d9efd0833bf7d08009e8cc58334ac
Pratyush2710 <pratyushhg@gmail.com>
2020-07-20 17:13:32 +0530

    Form changes completed

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,7 +1,37 @@
 import React from "react";
+import { MdSend } from "react-icons/md";
 
 const ExpenseForm = () => {
-  return <div>Hello from expense form</div>;
+  return (
+    <form>
+      <div className="form-center">
+        <div className="form-group">
+          <label htmlFor="charge">Charge</label>
+          <input
+            type="text"
+            className="form.control"
+            id="charge"
+            name="charge"
+            placeholder="expense name"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="amount">amount</label>
+          <input
+            type="text"
+            className="form.control"
+            id="amount"
+            name="amount"
+            placeholder="expense amount"
+          />
+        </div>
+      </div>
+      <button type="submit" className="btn">
+        submit
+        <MdSend className="btn-icon" />
+      </button>
+    </form>
+  );
 };
 
 export default ExpenseForm;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { MdEdit, MdDelete } from "react-icons/md";
 
-const ExpenseItem = () => {
-  return <div>Hello from expense Item</div>;
+const ExpenseItem = ({ expense }) => {
+  const { id, charge, amount } = expense;
+  return (
+    <li className="item">
+      <div className="info">
+        <span className="expense">{charge}</span>
+        <span className="amount">
+          {"\u20B9"}
+          {amount}
+        </span>
+      </div>
+      <div>
+        <button className="edit-btn" aria-label="edit button">
+          <MdEdit />
+        </button>
+        <button className="clear-btn" aria-label="delete button">
+          <MdDelete />
+        </button>
+      </div>
+    </li>
+  );
 };
 
 export default ExpenseItem;
